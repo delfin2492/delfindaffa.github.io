cb6aefac0619c02f1740af853a97faa6a6ac2a27
delfin2492 <delfindaffa@gmail.com>
2022-09-13 10:05:12 +0700

    first commit

---
+++ dist/js/script.js
@@ -0,0 +1,62 @@
+// hamburger
+const hamburger = document.querySelector("#hamburger");
+const navMenu = document.querySelector("#nav-menu");
+
+hamburger.addEventListener("click", function () {
+  hamburger.classList.toggle("hamburger-active");
+  navMenu.classList.toggle("hidden");
+});
+
+// klik di luar hamburger
+window.addEventListener("click", function (e) {
+  if (e.target !== hamburger && e.target !== navMenu) {
+    hamburger.classList.remove("hamburger-active");
+    navMenu.classList.add("hidden");
+  }
+});
+
+// Alert
+const alert = document.querySelector("#alert");
+
+alert.addEventListener("click", function () {
+  myAlert.classList.toggle("hidden");
+});
+
+//Navbar fixed
+window.onscroll = function () {
+  const header = document.querySelector("header");
+  const fixedNav = header.offsetTop;
+  const top = document.querySelector("#top");
+  if (window.pageYOffset > fixedNav) {
+    header.classList.add("navbar-fixed");
+    top.classList.remove("hidden");
+    top.classList.add("flex");
+  } else {
+    header.classList.remove("navbar-fixed");
+    top.classList.remove("flex");
+    top.classList.add("hidden");
+  }
+};
+
+// DARKMODE TOGGLE
+const darkToggle = document.querySelector("#dark-toggle");
+const html = document.querySelector("html");
+
+darkToggle.addEventListener("click", function () {
+  if (darkToggle.checked) {
+    html.classList.add("dark");
+    localStorage.theme = "dark";
+  } else {
+    html.classList.remove("dark");
+    localStorage.theme = "light";
+  }
+});
+
+// pindahkan posisi toggle sesuai mode
+if (localStorage.theme === "dark" || (!("theme" in localStorage) && window.matchMedia("(prefers-color-scheme: dark)").matches)) {
+  document.documentElement.classList.add("dark");
+  darkToggle.checked = true;
+} else {
+  document.documentElement.classList.remove("dark");
+  darkToggle.checked = false;
+}
---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  content: ["index.html"],
+  darkMode: "class",
+  theme: {
+    container: {
+      center: true,
+      padding: "16px",
+    },
+    extend: {
+      colors: {
+        primary: "#0e7490",
+        dark: "#0f172a",
+        secondary: "#64748b",
+      },
+      screens: {
+        "2xl": "1320px",
+      },
+    },
+  },
+  plugins: [require("@tailwindcss/typography")],
+};
