fed838739151ae9ca2ee68633bade0805b3f5d43
delfin2492 <delfindaffa@gmail.com>
2023-02-15 10:17:57 +0700

    pintu otomatis

--- main.js
+++ main.js
@@ -27,16 +27,19 @@ class MyFooter extends HTMLElement  {
           <h3 class="font-semibold text-xl text-white mb-5">Kategori Tulisan</h3>
           <ul class="text-slate-300">
             <li>
-              <a href="artikel2.html" class="inline-block text-base hover:text-primary mb-3">Pengembangan Sektor E-Tourism</a>
+              <a href="Etourism.html" class="inline-block text-base hover:text-primary mb-3">Pengembangan Sektor E-Tourism</a>
             </li>
             <li>
-              <a href="artikel.html" class="inline-block text-base hover:text-primary mb-3">Perkembangan Ekonomi Digital</a>
+              <a href="ekonomidigital.html" class="inline-block text-base hover:text-primary mb-3">Perkembangan Ekonomi Digital</a>
             </li>
             <li>
-              <a href="artikel3.html" class="inline-block text-base hover:text-primary mb-3">Transformasi Digital di Indonesia</a>
+              <a href="transformasidigital.html" class="inline-block text-base hover:text-primary mb-3">Transformasi Digital di Indonesia</a>
             </li>
             <li>
               <a href="smarthome.html" class="inline-block text-base hover:text-primary mb-3">Smarthome Sederhana Berbasis IoT</a>
+            </li>
+            <li>
+              <a href="gerbangotomatis.html" class="inline-block text-base hover:text-primary mb-3">Miniatur Gerbang Otomatis</a>
             </li>
           </ul>
       </div>
